ea22404aa2e50bd9c4984f162ecbf4232f41b3aa
Jairo Yamarte <jairoyamarte@gmail.com>
2022-10-09 21:02:37 -0400

    Update lista de ordenes

--- resources/views/order/list.blade.php
+++ resources/views/order/list.blade.php
@@ -8,38 +8,40 @@
         </div>
     </div>
     <div class="row">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($orders as $order)
+        <div class="container">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td scope="row">{{ $order->id }} </td>
-                        <td>{{ $order->customer_name }} </td>
-                        <td>{{ $order->customer_email }} </td>
-                        <td>{{ $order->customer_movile }} </td>
-                        @if ($order->status === 'CREATED')
-                            <td class="alert-warning">{{ $order->status }}</td>
-                        @elseif ($order->status === 'PAYED')
-                            <td class="alert-success">{{ $order->status }}</td>
-                        @else
-                            <td class="alert-danger">{{ $order->status }}</td>
-                        @endif
-                        
-                        <td>{{ $order->created_at }} </td>    
-                        <td><a href="#">Delete</a></td>
+                        <th scope="col">Id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Status</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($orders as $order)
+                        <tr>
+                            <td scope="row">{{ $order->id }} </td>
+                            <td>{{ $order->customer_name }} </td>
+                            <td>{{ $order->customer_email }} </td>
+                            <td>{{ $order->customer_mobile }} </td>
+                            <td>{{ $order->created_at }} </td>    
+                            @if ($order->status === 'APPROVED')
+                                <td class="alert-success">{{ $order->status }}</td>
+                            @elseif ($order->status === 'PENDING')
+                                <td class="alert-warning">{{ $order->status }}</td>
+                            @else
+                                <td class="alert-danger">{{ $order->status }}</td>
+                            @endif
+                            
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        
     </div>
 </div>
 
