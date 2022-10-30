def7b3cbd17f0ecfa983761b40dd61ad5ac1f5b6
Jairo Yamarte <jairoyamarte@gmail.com>
2022-10-30 10:51:27 -0400

    Install vue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.principal')
-@section('info')
+ @section('info')
     <div class="container-fluid">
         <hr>
         <div class="row text-center">
             <h1 class="display-1">La Tiendita</h1>
         </div>
         <div class="row">
-        <figure class="text-end">
-            <blockquote class="blockquote">
-                <p>Hacia ti, Señor, miro sulicante;hacia ti, que reinas en el cielo.</p>
-            </blockquote>
-            <figcaption class="blockquote-footer">
-                Salmos <cite title="Source Title">123:1</cite>
-            </figcaption>
-        </figure>
-        </div>
+            <figure class="text-end">
+                <blockquote class="blockquote">
+                    <p>Hacia ti, Señor, miro sulicante;hacia ti, que reinas en el cielo.</p>
+                </blockquote>
+                <figcaption class="blockquote-footer">
+                    Salmos <cite title="Source Title">123:1</cite>
+                </figcaption>
+            </figure>
+        </div>        
     </div>
-@endsection
+    
