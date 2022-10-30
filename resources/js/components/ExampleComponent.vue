018ff6d8f1af59164eb576e7075d4796087f3a04
Jairo Yamarte <jairoyamarte@gmail.com>
2022-10-30 11:22:50 -0400

    organized archive

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,7 +1,5 @@
 <template>
-    <div>
-        <h1> Hola Mundo</h1>
-    </div>
+    
 </template>
 
 <script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,4 @@
 @extends('layouts.principal')
- @section('info')
-    <div class="container-fluid">
-        <hr>
-        <div class="row text-center">
-            <h1 class="display-1">La Tiendita</h1>
-        </div>
-        <div class="row">
-            <figure class="text-end">
-                <blockquote class="blockquote">
-                    <p>Hacia ti, Señor, miro sulicante;hacia ti, que reinas en el cielo.</p>
-                </blockquote>
-                <figcaption class="blockquote-footer">
-                    Salmos <cite title="Source Title">123:1</cite>
-                </figcaption>
-            </figure>
-        </div>        
-    </div>
+@section('info')
     
+@endsection
